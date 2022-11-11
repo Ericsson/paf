8b68fb147fb5758029c273e451451d7eec338135
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2022-11-11 17:40:17 +0100

    Update meta data for 1.0.4
    Update version information in preparation for the 1.0.4 release.
    
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>

--- paf/server.py
+++ paf/server.py
@@ -17,7 +17,7 @@ from paf.logging import LogCategory, debug, info, warning
 
 MAJOR_VERSION = 1
 MINOR_VERSION = 0
-PATCH_VERSION = 3
+PATCH_VERSION = 4
 
 VERSION = "%d.%d.%d" % (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
